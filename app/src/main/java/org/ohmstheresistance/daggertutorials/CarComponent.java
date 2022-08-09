e9fbff095005ae0f93e9ce3dcb29500671175f84
InquisitiveMindHasToKnow <raymondomardev@gmail.com>
2022-08-08 21:14:11 -0400

    Field/Member Variable Injection

--- app/src/main/java/org/ohmstheresistance/daggertutorials/CarComponent.java
+++ app/src/main/java/org/ohmstheresistance/daggertutorials/CarComponent.java
@@ -6,4 +6,8 @@ import dagger.Component;
 public interface CarComponent {
 
     Car getCar();
+
+    //inject the activity you want to use..MainActivity in this eg.
+    //Each activity needs its own inject method. (name of method does not matter.)
+    void inject(MainActivity mainActivity);
 }
